873ed77930a113ba133b2e49e13fd66ff30979ad
Giovanni Lela <giovanni.lela@gmail.com>
2017-11-09 17:42:45 +0100

    cleanup

--- scripts/wit.js
+++ scripts/wit.js
@@ -1,74 +1,71 @@
 require('dotenv').config()
 const google = require('../lib/google')
 const faq = require('../lib/faq')
-
 const CONFIDENCE_THRESHOLD = 0.75
-const path = require('path')
 
 /**
  * Main bot init
  */
-module.exports = async function(robot) {
+module.exports = async function (robot) {
   if (!process.env.HUBOT_WIT_TOKEN) {
     console.log('No wit token')
     return
   }
-  await google.init();
+  await google.init()
 
   robot.respond(/(.*)/, async function (msg) {
     const query = msg.match[1]
 
     try {
       const intent = await getIntent(query, robot)
-      if(intent === null) {
+      if (intent === null) {
         return msg.reply('Non ho capito')
       }
 
       const {intentName, entities} = intent
       if (intentName === 'richiesta ferie') {
+        const {datetime} = entities
 
-        if(!entities.interval){
+        if (!datetime) {
           return msg.reply('Non ho capito le date')
         }
 
-        const to = new Date(entities.interval[0].to.value)
-        const from = new Date(entities.interval[0].from.value)
+        const to = new Date(datetime[0].to.value)
+        const from = new Date(datetime[0].from.value)
         const title = 'Ferie ' + msg.message.user.name
 
-        const r = await google.addEvent(title, from, to)
+        await google.addEvent(title, from, to)
         return msg.reply('Ok fatto, ho segnato sul calendario la tua richiesta, sarai ricontatto/a')
       }
 
-      const response = faq[intentName] || 'Non so nulla dell argomento ' + intentName;
+      const response = faq[intentName] || 'Non so nulla dell argomento ' + intentName
       msg.reply(response)
-
-    } catch (e) { 
+    } catch (e) {
       msg.reply(`è successo qualcosa di strano: ${e.message}`)
-      console.log(e) 
+      console.log(e)
     }
   })
 }
 
 const getIntent = (query, robot) => new Promise((resolve, reject) => {
   robot.http(`https://api.wit.ai/message?v=20171029&session_id=123&q=${encodeURI(query)}`)
-  .header('Content-Type', 'application/json')
-  .header('Authorization', 'Bearer ' + process.env.HUBOT_WIT_TOKEN)
-  .header('Accept', 'application/vnd.wit.20141022+json')
-  .get()((err, r, body) => {
-    if (err) reject(err)
+    .header('Content-Type', 'application/json')
+    .header('Authorization', 'Bearer ' + process.env.HUBOT_WIT_TOKEN)
+    .header('Accept', 'application/vnd.wit.20141022+json')
+    .get()((err, r, body) => {
+      if (err) reject(err)
 
-    const res = JSON.parse(body)
+      const res = JSON.parse(body)
 
-    if (!res.entities.intent) {
-      return resolve(null);
-    }
+      if (!res.entities.intent) {
+        return resolve(null)
+      }
 
-    const {confidence, value} = res.entities.intent[0]
-    if (confidence < CONFIDENCE_THRESHOLD) {
-      console.log(confidence, value);
-      return resolve(null);
-    }
+      const {confidence, value} = res.entities.intent[0]
+      if (confidence < CONFIDENCE_THRESHOLD) {
+        return resolve(null)
+      }
 
-    return resolve({intentName: value, entities: res.entities});
-  }) 
+      return resolve({intentName: value, entities: res.entities})
+    })
 })
